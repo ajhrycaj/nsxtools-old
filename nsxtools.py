821529531ec45ffd341dadbe2004097247fb293f
ajhrycaj <Redwings2>
2018-03-28 13:34:00 -0400

    Initial Commit

--- nsxtools.py
+++ nsxtools.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html',name='Main Page')
 
+@app.route('/contact')
+def contact():
+    return 'Contact info here'
+
+@app.route('/listnsxmanager')
+def getNsxManagerFromDB():
+    return 'test'
+app.add_url_rule('/listnsxmanager','listnsxmanager',getNsxManagerFromDB)
+
+@app.route('/addnsxmanager')
+def addNsxManagerToDB():
+    return 'Add NSX Manager'
+app.add_url_rule('/addnsxmanager','addnsxmanager',addNsxManagerToDB)
 
 if __name__ == '__main__':
     app.run()
